836a266eb49d2b0d341fedbff8f24ba0e24aea0f
Ajinesh Pratap Singh <152050389+ajinesh703@users.noreply.github.com>
2024-07-10 12:42:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+class App extends React.Component {
+  state = { advice: '' };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://official-joke-api.appspot.com/random_joke')
+      .then((response) => {
+        const { advice } = response.data.slip;
+
+        this.setState({ advice });
+
+
+      })
+      .catch((error) => {
+        console.log(error);
+
+      });
+
+  }
+
+  render() {
+    const { advice } = this.state;
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button className="button">
+            <span>CLICK A JOKE!</span>
+          </button>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+
+export default App;
